7bd60e74614f3efa89312966093c0eabf185d5d7
Doman Szabo <118120551+DomanSzabo@users.noreply.github.com>
2024-06-13 18:49:34 +0200

    commenting until middle of rounded make function

--- modButtons.py
+++ modButtons.py
@@ -1,6 +1,9 @@
 from gturtle import *
+#erstellen von Ursprungs - Class von allen Buttons (alle zukünftigen Buttons werden (teilweise über mehrere Ecken) manche dieser properties, Funktionen, etc. vererben) 
 class Button(object):
+    #die buttons class bekommt eine Liste (Buttons) zugewiesen, in der in Zukunft alle Buttons gespeichert werden
     buttons = []
+    #die init Funktion wird bei der Initialisierung von jedem Objekt der class aufgerufen und in ihr werden alle properties dem Objekt zugeordnet
     def __init__(self, posX, posY, width, height, color, text, radius):
         self.posX = posX
         self.posY = posY
@@ -15,11 +18,12 @@ class Button(object):
         setPenWidth(3)
         setPenColor(self.color)
         setFillColor(self.color)
-        
+        #das Objekt fügt sich selber der Liste an Buttons an
         self.buttons.append(self)
+        #führt die eigene Funktion aus (s.u.), um sich selbst zu erstellen
         self.rounded_make()
         
-        
+    #veraltete Funktion zum erstellen der selbst, wird nicht verwendet, also auch nicht kommentiert
     def make(self):
         setFontSize(24)
         heading(0)
@@ -41,11 +45,13 @@ class Button(object):
         
         label(self.text, adjust = "c")
         
+    #diese Funktion wir verwendet, um die Buttons zu erstellen (mit abgerundeten ecken)
     def rounded_make(self):
         setFontSize(24)
         heading(-90)
         pd()
         setPos(self.posX, self.posY)
+        #zentrieren des Buttons auf der X, aber nicht auf der y achse
         fd(self.width / 2 - self.radius)
         for _ in range(23):
             fd((self.radius * 3.14159 / 180) * 3.91)
